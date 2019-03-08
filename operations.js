55fc78a3ce2437d3511948237e77ce7c6c1a01f7
Elie Schoppik <eschoppik@gmail.com>
2019-03-07 16:07:27 -0800

    init

---
+++ operations.js
@@ -0,0 +1,13 @@
+function add(a=0,b=0){
+  return a+b;
+}
+
+function average(...nums){
+  if(nums.length === 0) return 0;
+  const total = nums.reduce(function(acc,next){ 
+    return acc + next
+  }) 
+  return total / nums.length
+}
+
+module.exports = {add, average}
