23d29f7c8a68fd83a3247d061b0207b9dff31b87
Matt Lane <lane.matthew@gmail.com>
2019-05-07 15:37:57 -0700

    adding subtract

--- operations.js
+++ operations.js
@@ -2,6 +2,10 @@ function add(a = 0, b = 0) {
   return a + b;
 }
 
+function subtract(a = 0, b = 0) {
+  return a - b;
+}
+
 function average(...nums) {
   if (nums.length === 0) return 0;
   const total = nums.reduce(function(acc, next) {
