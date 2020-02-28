3c632c12c7266e2866ee44d2c878d7c84a185230
Colt Steele <colt@steele.com>
2020-02-28 13:19:24 -0800

    add new expectation

--- operations.test.js
+++ operations.test.js
@@ -1,36 +1,37 @@
 const { add, average, multiply, subtract } = require("./operations");
 
-describe("#add", function() {
-  it("adds numbers", function() {
+describe("#add", function () {
+  it("adds numbers", function () {
     expect(add(2, 2)).toEqual(4);
+    expect(add(4, 5)).toEqual(9);
   });
-  it("handles empty inputs", function() {
+  it("handles empty inputs", function () {
     expect(add()).toEqual(0);
   });
 });
 
-describe("#subtract", function() {
-  it("subtracts numbers", function() {
+describe("#subtract", function () {
+  it("subtracts numbers", function () {
     expect(subtract(2, 2)).toEqual(0);
   });
-  it("handles empty inputs", function() {
+  it("handles empty inputs", function () {
     expect(subtract()).toEqual(0);
   });
 });
 
-describe("#multiply", function() {
-  it("multiplies two numbers", function() {
+describe("#multiply", function () {
+  it("multiplies two numbers", function () {
     expect(multiply(2, 3)).toEqual(6);
     expect(multiply(-4, 5)).toEqual(-20);
   });
 });
 
-describe("#average", function() {
-  it("calculates the average", function() {
+describe("#average", function () {
+  it("calculates the average", function () {
     expect(average(2, 2)).toEqual(2);
     expect(average(2, -2)).toEqual(0);
   });
-  it("handles empty inputs", function() {
+  it("handles empty inputs", function () {
     expect(average()).toEqual(0);
   });
 });
